3eb8fdd8a2e8c7281b9c5a0c3c18baf7e84e2613
Alejandro <alexsotog82@gmail.com>
2020-05-08 01:09:30 -0500

    se sube ejercios de pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes-gym';
+  nombre: string = 'Alejandro';
+  name2: string = 'alejAndro soTo';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.235;
+  moneda: number = 12345;
+  fecha = new Date();
+  activar: boolean = true;
+  hero = {
+    name: 'logan',
+    clave: 'Wolverine',
+    age: '500',
+    adress: {
+      street: 'Primera',
+      house: 20
+    }
+  };
+  vidioUrl: string = 'https://www.youtube.com/embed/S0tltAWkfLA';
+
+  valuePromise = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
 }
---
+++ src/app/pipes/visualizar.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { VisualizarPipe } from './visualizar.pipe';
+
+describe('VisualizarPipe', () => {
+  it('create an instance', () => {
+    const pipe = new VisualizarPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
